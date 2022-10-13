4dd6f3e7bdf6658a6d448db853c6985df8fcb1b5
calpiconoko <klutsumi@ucsc.edu>
2022-10-12 23:41:41 -0700

    done

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+/**
+* Summary.
+*
+* Description.
+*
+* @file This files defines the MyClass class.
+* @author @Keiko @Kenice
+* @since 10.12.22
+// Prompt user to insert digit
+// Take user input
+// Accept user's input only if it's a number
+// Multiply user's number by 2
+// Display the product
