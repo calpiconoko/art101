df2b40e671e405138213cb5564fb58c38456d203
calpiconoko <klutsumi@ucsc.edu>
2022-10-12 20:21:09 -0700

    k

---
+++ lab3/js/lab.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kenice & Keiko Homepage</title>
+	<link rel="stylesheet" type="text/css" href="css/site.css">
+</head>
+<body class ="body">
+	<main>
+    <h1>Homepage</h1>
+    <div class="split left">
+      <p>Hello, my name is Kenice and I am a fourth year AGPM major I love games but I am terrible at them. My favorite is probably Breath of the Wild as of now.</p>
+		  <img id="pic" src="img/me.jpg">
+    </div>
+    <div class="split right">
+  		<p>Hi, I'm Keiko! I'm a second year studying critical race and ethnic studies. I usually have an anti-technology mentality but this class is really fun so far.</p>
+			<img id="pic" src="img/selfie.jpg">
+  	</div>
+
+    <div class="assignments">
+      <h2>Labs<h2>
+      <ul>
+        <li><a href="lab2/index.html">Lab2: Your first HTML</a></li>
+        <li><a href="lab2/index.html">Lab3: File Structure</a></li>
+        <li><a href="lab2/index.html">Lab4: File Transfer</a></li>
+        <li><a href="lab2/index.html">Lab5:</a></li>
+        <li><a href="lab2/index.html">Lab6:</a></li>
+    </div>
+	</main>
+
+
+</body>
+</html>
